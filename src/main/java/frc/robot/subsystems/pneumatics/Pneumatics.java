09fd8355d11f28c008f3c251815724b173f5c4c8
DuckDuckWhale <theduckyjason@gmail.com>
2019-01-18 20:27:01 -0500

    [Fix] Fix pneumatics

--- src/main/java/frc/robot/subsystems/pneumatics/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/pneumatics/Pneumatics.java
@@ -10,13 +10,13 @@ public final class Pneumatics extends Subsystem {
 	public static final DoubleSolenoid.Value RETRACT = Value.kReverse;
 	public static final DoubleSolenoid.Value OFF = Value.kOff;
 
-	public static final int SHIFTER_EXTEND = 1;
-	public static final int SHIFTER_RETRACT = 0;
-	public static final int PCM = 0; // Pneumatics control module
+	public static final int SHIFTER_EXTEND = 0;
+	public static final int SHIFTER_RETRACT = 7;
+	public static final int PCM = 1; // Pneumatics control module
 
 	@SuppressWarnings("unused")
 	private Compressor compressor = new Compressor(PCM);
-	private DoubleSolenoid shifter = new DoubleSolenoid(SHIFTER_EXTEND, SHIFTER_RETRACT);
+	private DoubleSolenoid shifter = new DoubleSolenoid(PCM, SHIFTER_EXTEND, SHIFTER_RETRACT);
 
 	public void shift(DoubleSolenoid.Value v) {
 		shifter.set(v);
