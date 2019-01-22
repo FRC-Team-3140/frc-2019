dd1f24935596726a26dcbab482780422eef9afe8
encrxpted <rulangu@gmail.com>
2019-01-22 17:48:00 -0500

    forgot to commit this file oops

--- src/main/java/frc/robot/commands/pneumatics/tilter/ArmClose.java
+++ src/main/java/frc/robot/commands/pneumatics/tilter/ArmClose.java
@@ -4,11 +4,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.subsystems.pneumatics.Pneumatics;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
-public class TiltUp extends CommandGroup {
+public class ArmClose extends CommandGroup {
   
-  public TiltUp() {
-      addSequential(new Tilt(Pneumatics.RETRACT));
+  public ArmClose() {
+      addSequential(new Arm(Pneumatics.RETRACT));
       addSequential(new WaitCommand(0.3));
-      addSequential(new Tilt(Pneumatics.OFF));
+      addSequential(new Arm(Pneumatics.OFF));
   }
 }
