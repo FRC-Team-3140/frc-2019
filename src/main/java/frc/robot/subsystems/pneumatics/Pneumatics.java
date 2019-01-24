f0f932f3015db2cc0c33eca137b5744afefeb948
5ARAH101 <sarhuang101@gmail.com>
2019-01-24 16:15:46 -0500

    Tested arm- it is working

--- src/main/java/frc/robot/subsystems/pneumatics/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/pneumatics/Pneumatics.java
@@ -10,10 +10,11 @@ public final class Pneumatics extends Subsystem {
 	public static final DoubleSolenoid.Value RETRACT = Value.kReverse;
 	public static final DoubleSolenoid.Value OFF = Value.kOff;
 
-	public static final int SHIFTER_EXTEND = 0;
-	public static final int SHIFTER_RETRACT = 7;
-	public static final int TILTER_EXTEND = 1;
-	public static final int TILTER_RETRACT = 6;	
+	// 0 and 7 are the current ports on chassis bot
+	public static final int SHIFTER_EXTEND = 1;
+	public static final int SHIFTER_RETRACT = 6;
+	public static final int ARM_EXTEND = 1;
+	public static final int ARM_RETRACT = 6;	
 	public static final int PUNCHER = 2;
 	public static final int CLIMBER_EXTEND = 1;
 	public static final int CLIMBER_RETRACT = 6;
@@ -22,7 +23,7 @@ public final class Pneumatics extends Subsystem {
 	@SuppressWarnings("unused")
 	private Compressor compressor = new Compressor(PCM);
 	private DoubleSolenoid shifter = new DoubleSolenoid(PCM, SHIFTER_EXTEND, SHIFTER_RETRACT);
-	private DoubleSolenoid arm = new DoubleSolenoid(PCM, TILTER_EXTEND, TILTER_RETRACT);
+	private DoubleSolenoid arm = new DoubleSolenoid(PCM, ARM_EXTEND, ARM_RETRACT);
 	private Solenoid puncher = new Solenoid(PCM, PUNCHER);
 	private DoubleSolenoid climber = new DoubleSolenoid(PCM, CLIMBER_EXTEND, CLIMBER_RETRACT);
 
