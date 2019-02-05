f8193f4ff8e63167a800bed92254b1277b6e9c61
encrxpted <rulangu@gmail.com>
2019-02-05 18:49:58 -0500

    [Feature] Add tunable ramp rate control

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,27 @@
 package frc.robot;
 
 public interface Constants {
+    /*
+     * SUBSYSTEMS
+     */
+     // DRIVETRAIN
+     // the following comments are the mechanical markers
+	public final int LEFT_DRIVE_MASTER = 2, // 6
+        LEFT_DRIVE_SLAVE1 = 3, // 5
+        LEFT_DRIVE_SLAVE2 = 4, // 4
+        RIGHT_DRIVE_MASTER = 5, // 3
+        RIGHT_DRIVE_SLAVE1 = 6, // 2
+        RIGHT_DRIVE_SLAVE2 = 7; // 1
 
+    // PNEUMATICS
+    public static final int SHIFTER_EXTEND = 0;
+	public static final int SHIFTER_RETRACT = 7;
+	public static final int PCM = 1; // Pneumatics control module
+
+    
+    /*
+     * SENSORS
+     */
     // ENCODER
     public static final int COUNTS_PER_REV = 42;
 
--- src/main/java/frc/robot/subsystems/pneumatics/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/pneumatics/Pneumatics.java
@@ -3,16 +3,13 @@ package frc.robot.subsystems.pneumatics;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Constants;
 
-public final class Pneumatics extends Subsystem {
+public final class Pneumatics extends Subsystem implements Constants {
 
 	public static final DoubleSolenoid.Value EXTEND = Value.kForward;
 	public static final DoubleSolenoid.Value RETRACT = Value.kReverse;
 	public static final DoubleSolenoid.Value OFF = Value.kOff;
-
-	public static final int SHIFTER_EXTEND = 0;
-	public static final int SHIFTER_RETRACT = 7;
-	public static final int PCM = 1; // Pneumatics control module
 
 	@SuppressWarnings("unused")
 	private Compressor compressor = new Compressor(PCM);
